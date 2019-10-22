00170c26457632876aabd7d7e200a1ba38e9a09e
dinzhenggang@beyondcent.com <dinzhenggang@beyondcent.com>
2019-10-22 16:35:56 +0800

    fix: 调整代码
    调整代码

---
+++ src/main/java/com/dzg/project/BasicRunApplication.java
@@ -0,0 +1,18 @@
+package com.dzg.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 标准Demo项目
+ *
+ * @author DZG
+ */
+@SpringBootApplication
+public class BasicRunApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BasicRunApplication.class, args);
+  }
+
+}
