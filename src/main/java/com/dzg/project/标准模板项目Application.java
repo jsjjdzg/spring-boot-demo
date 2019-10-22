52d689dc0fba3740784911980aca40b0d0c33ef3
dinzhenggang@beyondcent.com <dinzhenggang@beyondcent.com>
2019-10-22 16:08:01 +0800

    fix: 初始化项目
    初始化项目

---
+++ src/main/java/com/dzg/project/标准模板项目Application.java
@@ -0,0 +1,13 @@
+package com.dzg.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 标准模板项目Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(标准模板项目Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dzg/project/标准模板项目ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzg.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 标准模板项目ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
